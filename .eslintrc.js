8f18a86d378e1f336573439e7599ec4fbd436ae8
Swati Fursule <swati@silvertrak.com.au>
2018-07-13 11:59:21 +1000

    working code

---
+++ .eslintrc.js
@@ -0,0 +1,48 @@
+// https://eslint.org/docs/user-guide/configuring
+
+module.exports = {
+  root: true,
+  parserOptions: {
+    parser: 'babel-eslint'
+  },
+  env: {
+    //browser: true,
+  },
+  extends: [
+    // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
+    // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
+    'plugin:vue/essential', 
+    // https://github.com/standard/standard/blob/master/docs/RULES-en.md
+    'standard'
+  ],
+  // required to lint *.vue files
+  plugins: [
+    'vue'
+  ],
+  // add your custom rules here
+  rules: {
+    // allow async-await
+    'generator-star-spacing': 'off',
+    // allow debugger during development
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'indent' : 0,
+    'space-before-blocks' : 0,
+    'key-spacing' : 0,
+    'comma-spacing' : 0,
+    'semi': 0,
+    'space-before-function-paren': 0,
+    'brace-style': 0,
+    'keyword-spacing': 0,
+    'quotes': 0,
+    'camelcase': 0,
+    'space-infix-ops': 0,
+    'eqeqeq': 0,
+
+    'no-trailing-spaces':0,
+    'object-curly-even-spacing':0,
+    'spaced-comment':0,
+    'comma-dangle':0
+
+
+  }
+}
